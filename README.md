4fab891c524facb16a767dd063060d008173c173
emines0 <emin.sefa1@gmail.com>
2023-02-05 13:11:53 +0100

    get unique array of categories, display categories dynamically

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 - useState for menu items, categories
 - iterating over the categories data
--
+- filter items based on category
+- get unique array of categories from data.js
+- show menu categories dynamically
--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import Menu from './Menu'
 import Categories from './Categories'
 import items from './data'
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+/*
+ * Get unique value of categories from data.js
+ * First item will be all
+ */
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCategories)
 
   const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+      /*
+       * If category is all set menu items to items and display all items from data.js
+       */
+    }
     const newItems = items.filter((item) => item.category === category)
     setMenuItems(newItems)
   }
@@ -18,7 +31,7 @@ function App() {
         <div className='title'>
           <h2>our menu</h2>
           <div className='underline'></div>
-          <Categories filterItems={filterItems} />
+          <Categories filterItems={filterItems} categories={categories} />
           <Menu items={menuItems} />
         </div>
       </section>
